68f35b594f0486841d016adbbf631640426ab64a
juanSe756 <juansebastiangilborda@gmail.com>
2024-10-20 21:02:44 -0500

    register and login working

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -6,13 +6,18 @@ from services.auth_service import AuthenticationService, create_club_token
 router = APIRouter()
 
 auth_service = AuthenticationService()
+
+
 @router.post("/register")
 async def register_user(user: UserCreate):
     return auth_service.register_user(user)
 
+
 @router.get("/hola")
 async def hello():
     return "Hola!"
+
+
 @router.post("/login")
 async def login(credentials: UserCreate):
     user = auth_service.authenticate_user(credentials.user_name, credentials.user_password)
@@ -20,11 +25,12 @@ async def login(credentials: UserCreate):
         raise HTTPException(status_code=400, detail="Invalid credentials")
     return {"message": "Login successful", "user": user}
 
+
 @router.post("/token_club")
 async def token_club(token_data: TokenData):
     role_id = auth_service.get_role_in_club(token_data.user_id, token_data.club_id)
     participant_data = {"user": token_data.user_id,
-                    "role": role_id,
-                    "club": token_data.club_id}
+                        "role": role_id,
+                        "club": token_data.club_id}
     access_token = create_club_token(data=participant_data)
     return {"access_token": access_token, "token_type": "bearer"}
